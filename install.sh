de81a304b2c6ef9805255602725982b7aa9b2295
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2025-09-07 19:01:05 +0200

    fix

--- install.sh
+++ install.sh
@@ -670,7 +670,7 @@ app_env_setup(){
 
     local envfile="${APP_DIR}/.env"
     local lines=0
-    lines=$(wc -l "${envfile}" 2>/dev/null || echo 0)
+    lines=$(wc -l < "${envfile}" 2>/dev/null || echo 0)
     if (( lines > 4 )); then
       run "Removing the last 4 lines of the .env" bash -lc "head -n -4 '${envfile}' > '${envfile}.tmp' && mv -f '${envfile}.tmp' '${envfile}'"
     fi
